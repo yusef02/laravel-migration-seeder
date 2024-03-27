2992b550846a46f81d44b8589278eac5d221d88a
yusef02 <youssef.bouziane2402@gmail.com>
2024-03-27 22:09:49 +0100

    add array fillable to Train model

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -9,4 +9,8 @@ class Train extends Model
 {
     use HasFactory;
     protected $table = 'trains';
+
+    protected $fillable = [
+        'azienda', 'stazione_di_partenza', 'stazione_di_arrivo', 'orario_di_partenza', 'orario_di_arrivo', 'codice_treno', 'numero_carrozze', 'in_orario', 'cancellato'
+    ];
 }
